9643854e7a5490b8a4af3d7b21751df4f5cf86db
Dawid Bator <dawid.bathor@gmail.com>
2018-05-26 10:37:38 +0200

    Uporzadkowanie kodu, dodanie nowej grafiki oraz plikow audio. wersja v2

---
+++ js/kobra.js
@@ -0,0 +1,167 @@
+"use strict";
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// jednostka box
+const box = 32;
+
+// grafika
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// audio
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/hastalavista.mp3";
+eat.src = "audio/chomp.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// kobra tworzenie
+
+let snake = [];
+
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+
+// ciastuszka
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// wynik
+
+let score = 0;
+
+//kontrola kobry
+
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d != "RIGHT"){
+        left.play();
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        up.play();        
+        d = "UP";
+    }else if(key == 39 && d != "LEFT"){
+        right.play();        
+        d = "RIGHT";
+    }else if(key == 40 && d != "UP"){
+        down.play();        
+        d = "DOWN";
+    }
+}
+
+// kolizja
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+// print na ekran
+
+function draw(){
+    
+    ctx.drawImage(ground,0,0);
+    
+    for( let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0 )? "#09a72d" : "#00ff3a";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+        
+        ctx.strokeStyle = "#09a72d";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+    
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    // pozycja glowy stara
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // kierunek
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    
+    // Gdy kobra cos zje
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        eat.play();
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+        // nie nszczymy ogona
+    }else{
+        // usuwamy ogon
+        snake.pop();
+    }
+    
+    // dodaj nowa glowe
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    // koniec gry
+    
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(game);
+        dead.play();
+    }
+    
+    snake.unshift(newHead);
+    
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score,2*box,1.6*box);
+}
+
+//wola funkcje rysuj co 1.5s
+
+let game = setInterval(draw,150);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,213 +1,145 @@
-/**
- * pieces.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2017, Codrops
- * http://www.codrops.com
- */
+/*show/hide folders and cobra */
+
+function showDiv(){
+    document.getElementById('demo-grid').style.display = "block";
+    typewriter();
+}        
+function hideDiv(){
+    document.getElementById('demo-grid').style.display = "none";
+}       
+      
+        
+function showCont(){
+    document.getElementById('containeryyy').style.display = "block";
+}           
+function hideCont(){
+    document.getElementById('containeryyy').style.display = "none";
+}    
+        
+function showProj(){
+    document.getElementById('projectWindow').style.display = "block";
+}           
+function hideProj(){
+    document.getElementById('projectWindow').style.display = "none";
+}
+
+function myFunction() {
+    var x = document.getElementById("snake");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
+
+
+
+
+/* TYPEWRITER */
+        
+// set up text to print, each item in array is new line
+var aText = new Array(
+"Cześć,", 
+"nazywam się <span id=\"name\">Dawid Bator</span>",
+"w tym roku ukończyłem studia pierwszego stopnia z Informatyki Stosowanej. Obecnie jestem nieszczęśliwym bezrobotnym i szukam pracy jako...",
+"Junior frontend deweloper lub Junior webdeweloper",
+"Mieszkam w Starym Sączu (małe miasteczko obok Nowego Sącza)",
+"Jestem otwarty na pracę w większych miastach.",
+"Oglądam mnóstwo filmów i seriali",
+"Lubię pograć w gierki. Czasem, aż za dużo.",
+"Chodzę sobie po górach.",
+"Staram się zostać programistą, który wie co robi. Niestety aktualnie jestem tylko dobrym operatorem wyszukiwarki google\'a.",
+"\"Not sure if i\'m a good junior programmer",
+"or just good at googling\"",
+"Całe moje zainteresowanie się tworzeniem stron, zaczęło się końcem drugiego roku na studiach. Przez profesora który mi to najpierw dobrze obrzydził. Z czasem tworząc kolejne projekty na zaliczenie pokochałem to i zrozumiałem, że to nie tylko kodowanie, ale w jakimś sensie sztuka. Na 3 roku udało mi się rozpocząć staż w firmie która zajmowała się tworzeniem stron i portali internetowych. Mogłem zobaczyć jak wygląda to wszystko od środka. Bardzo się cieszyłem, gdy starsi koledzy dali mi możliwość zakodowania moich pierwszych stron. Poczułem się jak dziecko w sklepie z zabawkami. W trakcie 4 miesięcznego stażu stworzyłem kilka stron. Z biegiem czasu nie jestem z nich dumny ponieważ, zawsze bym coś chciał zmienić.",
+" <a href=\"projects.html\">Sprawdź moje projekty ></a>"
+
+);
+var iSpeed = 30; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
 {
-	// Helper vars and functions.
-	const is3DBuggy = navigator.userAgent.indexOf('Firefox') > 0;
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("consolebody");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
 
-	// From https://davidwalsh.name/javascript-debounce-function.
-	function debounce(func, wait, immediate) {
-		var timeout;
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
-			};
-			var callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
-	};
 
-	// from http://www.quirksmode.org/js/events_properties.html#position
-	const getMousePos = (e) => {
-		let posx = 0;
-		let posy = 0;
-		if (!e) {let e = window.event};
-		if (e.pageX || e.pageY) 	{
-			posx = e.pageX;
-			posy = e.pageY;
-		}
-		else if (e.clientX || e.clientY) 	{
-			posx = e.clientX + document.body.scrollLeft
-				+ document.documentElement.scrollLeft;
-			posy = e.clientY + document.body.scrollTop
-				+ document.documentElement.scrollTop;
-		}
-		return {
-			x : posx,
-			y : posy
-		};
-	}
 
-	class Pieces {
-		constructor(el, options) {
-			this.DOM = {};
-			this.DOM.el = el;
-			this.options = {
-				// Number of pieces / Layout (rows x cols).
-				pieces: {rows: 12, columns: 10},
-				hasTilt: false,
-				// Main image tilt: max and min angles.
-				tilt: {maxRotationX: -2, maxRotationY: 2, maxTranslationX: 4, maxTranslationY: -2},
-				delay: 0, // Number || Array(random number from [min,max], e.g. [0,300] would set a random delay per piece from 0 to 300)
-				// background image src.
-				bgimage: 'none',
-				// default animations:
-				animationDefaults: {
-					duration: 600,
-					easing: [0.2,1,0.3,1],
-					delay: (t,i) => {
-						return i*parseInt(t.dataset.delay);
-					},
-					translateX: (t,i) => { 
-						return t.dataset.column < this.getTotalColumns()/2 ? anime.random(50,100)+'px' : anime.random(-100,-50)+'px';
-					},
-					translateY: (t,i) => { 
-						return anime.random(-1000,-800)+'px';
-					},
-					opacity: {
-						value: 0,
-						duration: 600,
-						easing: 'linear'
-					}
-				}
-			};
-			Object.assign(this.options, options);
-			this.init();
-		}
-		init() {
-			// Window sizes.
-			this.win = {width: window.innerWidth, height: window.innerHeight};
-			// Container sizes.
-			this.dimensions = {width: this.DOM.el.offsetWidth, height: this.DOM.el.offsetHeight};
-			// Render all the pieces defined in the options.
-			this.layout();
-			// Init tilt.
-			if ( this.options.hasTilt ) {
-				this.initTilt();
-			}
-			// Init/Bind events
-			this.initEvents();
-		}
-		layout() {
-			// The source of the main image.
-			this.imgsrc = this.DOM.el.style.backgroundImage.replace('url(','').replace(')','').replace(/\"/gi, "");
-			// The background image.
-			this.DOM.el.style.backgroundImage = this.options.bgimage !== 'none' ? `url(${this.options.bgimage})` : 'none';
-			// Create the pieces and add them to the DOM (append it to the main element).
-			this.pieces = [];
-			for (let r = 0; r < this.options.pieces.rows; r++) {
-				for (let c = 0; c < this.options.pieces.columns; c++) {
-					const piece = this.createPiece(r,c);	
-					piece.style.backgroundPosition = `${-1*c*100}% ${-1*100*r}%`;
-					this.pieces.push(piece);
-				}
-			}
-		}
-		createPiece(row, column) {
-			const w = Math.round(this.dimensions.width/this.options.pieces.columns);
-			const h = Math.round(this.dimensions.height/this.options.pieces.rows);
-			const piece = document.createElement('div');
 
-			piece.style.backgroundImage = `url(${this.imgsrc})`;
-			piece.className = 'piece';
-			piece.style.width = `${w}px`;
-			piece.style.height = `${h}px`;
-			piece.style.backgroundSize = `${w*this.options.pieces.columns+4}px auto`;
-			piece.dataset.row = row;
-			piece.dataset.column = column;
-			piece.dataset.delay = this.options.delay instanceof Array ? anime.random(this.options.delay[0],this.options.delay[1]) : this.options.delay;
-			this.DOM.el.appendChild(piece);
-			this.DOM.el.style.width = `${w*this.options.pieces.columns}px`;
-			this.DOM.el.style.height = `${h*this.options.pieces.rows}px`;
 
-			return piece;
-		}
-		// Set the pieces background image.
-		setImage(imgsrc) {
-			this.imgsrc = imgsrc;
-			for(const piece of this.pieces) {
-				piece.style.backgroundImage = `url(${this.imgsrc})`;
-			}
-		}
-		initTilt() {
-			if ( is3DBuggy ) return;
-			this.DOM.el.style.transition = 'transform 0.2s ease-out';
-			this.tilt = true;
-		}
-		removeTilt() {
-			if ( is3DBuggy ) return;
-			this.tilt = false;
-		}
-		initEvents() {
-			// Mousemove event / Tilt functionality.
-			const onMouseMoveFn = (ev) => {
-				requestAnimationFrame(() => {
-					if ( !this.tilt ) {
-						if ( is3DBuggy ) {
-							this.DOM.el.style.transform = 'none';
-						}
-						return false;
-					}
-					const mousepos = getMousePos(ev);
-					const docScrolls = {left : document.body.scrollLeft + document.documentElement.scrollLeft, top : document.body.scrollTop + document.documentElement.scrollTop};
-					const mouseposScroll = { x : mousepos.x - docScrolls.left, y : mousepos.y - docScrolls.top };
-					const rotX = 2*this.options.tilt.maxRotationX/this.win.height*mouseposScroll.y - this.options.tilt.maxRotationX;
-					const rotY = 2*this.options.tilt.maxRotationY/this.win.width*mouseposScroll.x - this.options.tilt.maxRotationY;
-					const transX = 2*this.options.tilt.maxTranslationX/this.win.width*mouseposScroll.x - this.options.tilt.maxTranslationX;
-					const transY = 2*this.options.tilt.maxTranslationY/this.win.height*mouseposScroll.y - this.options.tilt.maxTranslationY;
 
-					this.DOM.el.style.transform = `perspective(1000px) translate3d(${transX}px, ${transY}px,0) rotate3d(1,0,0,${rotX}deg) rotate3d(0,1,0,${rotY}deg)`;
-				});
-			};
 
-			// Window resize.
-			const onResizeFn = debounce(() => {
-				this.win = {width: window.innerWidth, height: window.innerHeight};
-				this.DOM.el.style.width = this.DOM.el.style.height = '';
-				const elBounds = this.DOM.el.getBoundingClientRect();
-				this.dimensions = {width: elBounds.width, height: elBounds.height};
-				for (let i = 0, len = this.pieces.length; i < len; i++) {
-					const w = Math.round(this.dimensions.width/this.options.pieces.columns);
-					const h = Math.round(this.dimensions.height/this.options.pieces.rows);
-					const piece = this.pieces[i];
-					
-					piece.style.width = `${w}px`;
-					piece.style.height = `${h}px`;
-					piece.style.backgroundSize = `${w*this.options.pieces.columns+4}px auto`;
-					this.DOM.el.style.width = `${w*this.options.pieces.columns}px`;
-					this.DOM.el.style.height = `${h*this.options.pieces.rows}px`;
-				}
-			}, 20);
+/*CZAS I DATA*/
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10) 
+{
+    dd='0'+dd;
+} 
 
-			document.addEventListener('mousemove', onMouseMoveFn);
-			window.addEventListener('resize', (ev) => onResizeFn());
-		}
-		getTotalRows() {
-			return this.options.pieces.rows;
-		}
-		getTotalColumns() {
-			return this.options.pieces.columns;
-		}
-		animate(animeopts) {
-			animeopts = animeopts || this.options.animationDefaults;
-			let opts = {
-				targets: this.pieces
-			};
-			Object.assign(opts, animeopts);
-			anime.remove(this.pieces);
-			anime(opts);
-		}
-	};
-	window.Pieces = Pieces;
-};
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+    m = checkTime(m);
+
+    document.getElementById('czas').innerHTML = 
+        h + ':' + m +'</br>'+ yyyy+'-'+mm+'-'+dd; ;
+    var t = setTimeout(startTime, 500);
+    
+}        
+        
+function checkTime(i) {
+    if (i < 10) {i = "0" +i}; //daje zero do liczb <10
+    return i;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
